152f811a6a43353513552c74264d53de00e9f5ed
Talhaoui Saber <talhaoui.sabir@gmail.com>
2026-09-18 20:30:14 +0100

    clean up code

--- src/app/components/dashboard/add_client/index.js
+++ src/app/components/dashboard/add_client/index.js
@@ -1,6 +1,5 @@
 import AddClientForm from "./AddClientForm";
-import ClientAddressForm from "./ClientAddressForm";
 import ClientProfileAvatar from "./ClientProfileAvatar";
 import ActionsButtons from "./ActionsButtons";
 
-export { AddClientForm, ClientAddressForm, ClientProfileAvatar, ActionsButtons };
+export { AddClientForm, ClientProfileAvatar, ActionsButtons };
