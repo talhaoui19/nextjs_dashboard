ded66d8353ca35f2263fb1f0c5e8b1084080a4e2
Talhaoui Saber <talhaoui.sabir@gmail.com>
2026-09-20 18:25:25 +0100

    clean up code

--- src/app/dashboard/messages/page.jsx
+++ src/app/dashboard/messages/page.jsx
@@ -5,19 +5,29 @@ export default async function MessagesPage({ searchParams }) {
   const { clientId } = await searchParams;
 
   const clients = await getClients();
+  const latestMessages = await getLatestMessages();
 
-  const selectedClientId = clientId || clients[0]?._id;
+  const sortedClients = [...clients].sort((a, b) => {
+    return (
+      new Date(
+        latestMessages?.find((message) => message._id === b._id)?.createdAt,
+      ) -
+      new Date(
+        latestMessages?.find((message) => message._id === a._id)?.createdAt,
+      )
+    );
+  });
+
+  const selectedClientId = clientId || sortedClients[0]?._id;
 
   const messages = selectedClientId
     ? await getMessagesByClient(selectedClientId)
     : [];
 
-  const latestMessages = await getLatestMessages();
-
   return (
     <section className="flex h-screen min-h-0 bg-white animate-fade transition-all duration-300">
       <MessagesContent
-        clients={clients}
+        clients={sortedClients}
         messages={messages}
         selectedClientId={selectedClientId}
         latestMessages={latestMessages}
